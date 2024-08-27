a3de509239c68d468d57dee3b4eea13c8219e664
torie28 <pastoriejoe3@gmail.com>
2024-08-27 16:20:00 +0300

    debugging controller to have controller model

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Courses;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -13,7 +13,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        // $this->middleware('auth');
     }
 
     /**
@@ -23,6 +23,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $courses = Courses::all();
+
+        // Pass the data to the view
+        return view('index', compact('courses'));
     }
 }
