95549d3baed98a42a59118cc14816ffb14f68056
is-haka <isihakamabo5@gmail.com>
2024-09-06 10:52:42 +0300

    now routes are grouped by prefix names

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\IctController;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,6 +25,14 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/', [HomeController::class, 'index']);
-Route::get('categories/ict', [IctController::class, 'ict'])->name('categories.ict');
-Route::get('course/{id}', [CourseController::class, 'course'])->name('course');
-Route::get('/cart', [CartController::class, 'index'])->name('cart');
+
+//Course routes
+Route::prefix('course')->group( function () {
+    Route::get('categories/ict', [IctController::class, 'ict'])->name('categories.ict');
+    Route::get('course/{id}', [CourseController::class, 'course'])->name('course');
+});
+
+//Cart routes
+Route::prefix('cart')->group( function () {
+    Route::get('/cart', [CartController::class, 'index'])->name('cart');
+});
