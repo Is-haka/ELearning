f3755e3587a7f242baee1e22aa22904c7d4d4352
fabiomunez <fabiomunez5@gmail.com>
2024-09-06 11:10:15 +0300

    updated databases and added admin panel

--- database/migrations/2024_08_28_092215_create_instructor_table.php
+++ database/migrations/2024_08_28_092215_create_instructor_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('instructor', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreignId('categories_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('categories_id')->constrained('categories')->onDelete('cascade');
             $table->timestamps();
         });
     }
